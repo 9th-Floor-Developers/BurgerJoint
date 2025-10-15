84a332cbfa527c95cd46eb17f7e88fd5f111f339
Landen Ingerslev <lkingerslev@gmail.com>
2025-10-15 18:30:57 -0400

    Modified program to use assets/data.json instead of players_data.json, updated files accordingly

--- burger_joint/utils/database.py
+++ burger_joint/utils/database.py
@@ -1,17 +1,25 @@
 import json
 import os
+from pathlib import Path
+
 from burger_joint.model.player import Player
 
-playersDataPath = os.path.join('game_data', 'players_data.json')
+
+def json_path() -> str:
+	base_dir = Path(__file__).resolve().parent.parent
+	data_path = os.path.join(
+		base_dir, 'assets', 'data.json'
+	)
+	return data_path
 
 
+# TODO: update somehow with individual player data save/load
+#  instead of reading/writing ALL player data
 def load_players_data():
-	with open(playersDataPath, 'r') as f:
+	with open(json_path(), 'r') as f:
 		return [Player(**data) for data in json.load(f)]
 
 
-def save_players_data(data : list[Player]):
-	with open(playersDataPath, 'w') as f:
+def save_players_data(data: list[Player]):
+	with open(json_path(), 'w') as f:
 		json.dump([player.__dict__ for player in data], f, indent=4)
-
-
