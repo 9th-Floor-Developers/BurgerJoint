577fab0e41ddc881df044dc44c7e89c08942b1ab
petepoop <70098997+petepoop@users.noreply.github.com>
2025-10-15 12:57:59 -0400

    Added setup function to add command to the bot object

--- burger_joint/bot.py
+++ burger_joint/bot.py
@@ -1,10 +1,14 @@
+
 """Bot initialization, event loop, command loader"""
 
 
-@bot.event
-async def on_ready():
-	print('Burger Joint Bot Online')
-	
-	for guild in bot.guilds:
-		channel = guild.system_channel
-		await channel.send('Burger Joint Bot Online')
+def setup(bot):
+	@bot.event
+	async def on_ready():
+		print('Burger Joint Bot Online')
+		
+		for guild in bot.guilds:
+			channel = guild.system_channel
+			await channel.send('Burger Joint Bot Online')
+
+
--- burger_joint/main.py
+++ burger_joint/main.py
@@ -1,6 +1,7 @@
 """Entry point for discord bot"""
 
 import os
+from bot import setup
 
 from discord import Bot, Intents
 from dotenv import load_dotenv
@@ -8,6 +9,7 @@ from dotenv import load_dotenv
 
 def main():
 	bot: Bot = Bot(intents=Intents.all())
+	setup(bot)
 	load_dotenv()
 	bot.run(os.getenv('DISCORD_TOKEN'))
 
