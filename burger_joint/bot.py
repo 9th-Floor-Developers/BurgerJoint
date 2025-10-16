b074f1e1696eeac583b6bcbed6c6b7ecbe93ed6b
Landen Ingerslev <lkingerslev@gmail.com>
2025-10-15 22:35:36 -0400

    Added bot commands:
    - Added start command
    
    - Added rename command
    
    - Optimized embed on commands that use simple embeds

--- burger_joint/bot.py
+++ burger_joint/bot.py
@@ -1,16 +1,13 @@
 """Bot initialization, event loop, command loader"""
 
 import discord
-from discord import Bot, Intents
+from discord import ApplicationContext, Bot, Intents
 
-from burger_joint.cogs.player_commands import PlayerCommands
 from burger_joint.model import Player
 from burger_joint.utils import database
-from utils.embeds import get_status_embed
+from utils import embeds
 
 bot = Bot(intents=Intents.all())
-player_commands = PlayerCommands()
-bot.add_cog(player_commands)
 
 
 def setup() -> Bot:
@@ -26,10 +23,58 @@ async def on_ready():
 #	channel = guild.system_channel
 #	await channel.send('Burger Joint Bot Online')
 
-@bot.slash_command(description="Display your joint's status.")
-async def status(ctx: discord.ApplicationContext):
+@bot.slash_command(description='')
+async def start(ctx: ApplicationContext):
 	player: Player | None = database.get_player(ctx.author.id)
 	if not player:
-		player = player_commands.create_new_player(ctx.author)
+		database.create_new_player(ctx.author)
+		await ctx.respond(
+			embed=embeds.simple_embed(
+				'✅ You have successfully established your very own burger joint!'
+			)
+		)
+	else:
+		await ctx.respond(
+			embed=embeds.simple_embed(
+				f'🍔 {player.username} already owns a burger joint called "{player.shop_name}".',
+				embed_color=discord.Color.yellow()
+			)
+		)
+
+
+@bot.slash_command(description="Display your joint's status.")
+async def status(ctx: ApplicationContext):
+	player: Player | None = database.get_player(ctx.author.id)
+	if not player:
+		await ctx.respond(
+			embed=embeds.simple_embed(
+				'❌ You do not own a burger joint!',
+				'Use the `/start` command to start your very own burger joint!',
+				discord.Color.red()
+			)
+		)
+		return
 	
-	await ctx.respond(embed=get_status_embed(player))
+	await ctx.respond(embed=embeds.status_embed(player))
+
+
+@bot.slash_command(description='Rename your burger joint.')
+async def rename(ctx: ApplicationContext, new_name: str):
+	player: Player | None = database.get_player(ctx.author.id)
+	if not player:
+		await ctx.respond(
+			embed=embeds.simple_embed(
+				'❌ You do not own a burger joint!',
+				'Use the `/start` command to start your very own burger joint!',
+				discord.Color.red()
+			)
+		)
+		return
+	
+	player.shop_name = new_name
+	database.save_data(player)
+	await ctx.respond(
+		embed=embeds.simple_embed(
+			f'✅ Changed burger joint name to: {player.shop_name}!'
+		)
+	)
--- burger_joint/utils/embeds.py
+++ burger_joint/utils/embeds.py
@@ -1,33 +1,45 @@
 """Display various information as embeds."""
 
 import discord
-from discord import Embed
+from discord import Color, Embed, User
 
 from burger_joint.model import Player
 
 
-def get_status_embed(player: Player) -> Embed:
-    """Returns an embed displaying the player's stats with emojis."""
+def simple_embed(
+	title_text: str,
+	description_text: str = None,
+	embed_color: Color = Color.green()
+) -> Embed:
+	return Embed(
+		title=title_text,
+		description=description_text,
+		color=embed_color
+	)
 
-    embed = discord.Embed(
-        title=f"🍔 {player.shop_name} Status",
-        description=
-            f"🏆 Level: {player.level} | ✨ XP: {player.xp} | 💰 Balance: ${player.balance}"
-        ,
-        color=discord.Color.green()
-    )
-    
-    embed.add_field(
-	    name="💵 Burgers Sold",
-	    value=str(player.burgers_sold)
-    ).add_field(
-	    name="🛠️ Upgrades",
-	    value=str(len(player.upgrades))
-    ).add_field(
-	    name="👨‍🍳 Employees",
-	    value=str(len(player.employees))
-    ).set_footer(
-	    text=f"⭐ Prestige Level: {player.prestige}"
-    )
-    
-    return embed
+
+def status_embed(player: Player) -> Embed:
+	"""Returns an embed displaying the player's stats with emojis."""
+	
+	embed = Embed(
+		title=f"🍔 {player.shop_name} Status",
+		description=
+		f"🏆 Level: {player.level} | ✨ XP: {player.xp} | 💰 Balance: ${player.balance}"
+		,
+		color=discord.Color.green()
+	)
+	
+	embed.add_field(
+		name="💵 Burgers Sold",
+		value=str(player.burgers_sold)
+	).add_field(
+		name="🛠️ Upgrades",
+		value=str(len(player.upgrades))
+	).add_field(
+		name="👨‍🍳 Employees",
+		value=str(len(player.employees))
+	).set_footer(
+		text=f"⭐ Prestige Level: {player.prestige}"
+	)
+	
+	return embed
