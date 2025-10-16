9773b4485197fe99348d4d7b730c65df0272ad45
Landen Ingerslev <lkingerslev@gmail.com>
2025-10-16 14:33:53 -0400

    Fixed enums not saving bug with new and improved saving system
    - Manually set player badges to a set because json cannot comprehend sets
    
    - Serialize each attribute of player with the new json_convert() function that runs on user save.
    
    - Updated save_data() to be more efficient and modular. Changed some variable names.

--- burger_joint/utils/database.py
+++ burger_joint/utils/database.py
@@ -1,11 +1,13 @@
 import json
 import os
+from enum import Enum
 from pathlib import Path
 from typing import Any
 
 from discord import User
 
 from burger_joint.model.player import Player
+from burger_joint.utils import BadgeID
 
 
 def json_path() -> str:
@@ -15,35 +17,58 @@ def json_path() -> str:
 	)
 	return data_path
 
+
 def create_new_player(user: User):
 	player = Player(
 		user_id=user.id, username=user.name,
 		shop_name=f"{user.name}'s Burger Joint", balance=100, level=1,
-		xp=0, burgers_sold=0, upgrades=[], employees=[], badges=[],
+		xp=0, burgers_sold=0, upgrades=[], employees=[], badges=set(),
 		prestige=0
 	)
 	save_data(player)
 	return player
 
+
 def get_player(id: int) -> Player | None:
 	with open(json_path()) as f:
 		file: list[dict[str, Any]] = json.load(f)
+		
 		for player_json in file:
 			if player_json['user_id'] == id:
-				return Player(**player_json)
+				player = Player(**player_json)  # sets badges as type list
+				
+				player.badges = {
+					BadgeID(badge)
+					for badge in player.badges
+				}
+				
+				return player
 	return None
+
+
+def json_convert(obj: Any):
+	if isinstance(obj, Enum):
+		return obj.value
+	if isinstance(obj, set):
+		return list(obj)
+	if hasattr(obj, "__dict__"):
+		return obj.__dict__
+	return obj
 
 
 def save_data(player: Player) -> None:
 	with open(json_path(), 'r+') as f:
-		file: list[dict[str, Any]] = json.load(f)
+		non_user_data: list[dict[str, Any]] = json.load(f)
 		
-		for data in file:
-			if data['user_id'] == player.user_id:
-				file.remove(data)
-				break
+		non_user_data = [data for data in non_user_data if
+			data['user_id'] != player.user_id]
 		
-		file.append(player.__dict__)
+		user_data = json.loads(
+			json.dumps(player.__dict__, default=json_convert)
+		)
+		
+		non_user_data.append(user_data)
+		
 		f.seek(0)
-		json.dump(file, f, indent=4)
+		json.dump(non_user_data, f, indent=4)
 		f.truncate()
