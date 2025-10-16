37bc749c903ab8b647fea7911a16fe6faf39ea8a
Landen Ingerslev <lkingerslev@gmail.com>
2025-10-15 22:37:14 -0400

    Changed functionality of database.py:
    - Refactored init_player() to create_new_player()
    
    - Changed data saving to delete current user data and replace it with new user data. Bug was just adding new user  data to end of file and not updating.

--- burger_joint/utils/database.py
+++ burger_joint/utils/database.py
@@ -3,6 +3,8 @@ import os
 from pathlib import Path
 from typing import Any
 
+from discord import User
+
 from burger_joint.model.player import Player
 
 
@@ -13,6 +15,15 @@ def json_path() -> str:
 	)
 	return data_path
 
+def create_new_player(user: User):
+	player = Player(
+		user_id=user.id, username=user.name,
+		shop_name=f"{user.name}'s Burger Joint", balance=100, level=1,
+		xp=0, burgers_sold=0, upgrades=[], employees=[], badges=[],
+		prestige=0
+	)
+	save_data(player)
+	return player
 
 def get_player(id: int) -> Player | None:
 	with open(json_path()) as f:
@@ -26,6 +37,12 @@ def get_player(id: int) -> Player | None:
 def save_data(player: Player) -> None:
 	with open(json_path(), 'r+') as f:
 		file: list[dict[str, Any]] = json.load(f)
+		
+		for data in file:
+			if data['user_id'] == player.user_id:
+				file.remove(data)
+				break
+		
 		file.append(player.__dict__)
 		f.seek(0)
 		json.dump(file, f, indent=4)
