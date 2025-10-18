a99c6145be393301b41b920adf645d5b3fe5db4b
Landen Ingerslev <lkingerslev@gmail.com>
2025-10-18 00:03:08 -0400

    Cost check decorator

--- burger_joint/cogs/minigames.py
+++ burger_joint/cogs/minigames.py
@@ -1,10 +1,61 @@
+import functools
+import random
+
 import discord
-from discord import ApplicationContext, Bot, Cog
+from discord import ApplicationContext, Bot, Cog, Option
 
 from bot import player_check
 from burger_joint.cogs.mini_games.blackjack import BlackJack
 from burger_joint.model import Player
 from burger_joint.utils import embeds
+
+
+def cost_check(needed: int = 1, extra: bool = False):
+	def decorator(func):
+		@functools.wraps(func)
+		async def wrapper(self, ctx: ApplicationContext, *args, **kwargs):
+			player: Player = ctx.player  # type: ignore
+			if player.balance < 1:
+				await ctx.respond(
+					embed=embeds.simple_embed(
+						description_text='🏦 You do not have any money.',
+						embed_color=discord.Color.red()
+					)
+				)
+				return
+			elif player.balance < needed:
+				await ctx.respond(
+					embed=embeds.simple_embed(
+						description_text=f'💵 You need at least ${needed} (${needed - player.balance} more).',
+						embed_color=discord.Color.red()
+					)
+				)
+				return
+			elif extra:
+				bet = list(kwargs.values())[0]
+				
+				if bet > player.balance:
+					await ctx.respond(
+						embed=embeds.simple_embed(
+							description_text=f'💰 You cannot spend more than you have',
+							embed_color=discord.Color.red()
+						)
+					)
+					return
+				elif bet < 1:
+					await ctx.respond(
+						embed=embeds.simple_embed(
+							description_text=f'💰 You cannot bet a negative number.',
+							embed_color=discord.Color.red()
+						)
+					)
+					return
+			
+			await func(self, ctx, *args, **kwargs)
+		
+		return wrapper
+	
+	return decorator
 
 
 class MiniGames(Cog):
@@ -13,27 +64,11 @@ class MiniGames(Cog):
 	
 	@discord.slash_command(description='')
 	@player_check
-	async def blackjack(self, ctx: ApplicationContext, bet: int):
-		player: Player = ctx.player
-		if player.balance < 1:
-			await ctx.respond(
-				embed=embeds.simple_embed(
-					description_text='🏦 You do not have enough money to play blackjack',
-					embed_color=discord.Color.red()
-				)
-			)
-			return
-		elif bet <= 0:
-			await ctx.respond(
-				embed=embeds.simple_embed(
-					description_text='💵 Minimum Bet is $1',
-					embed_color=discord.Color.red()
-				)
-			)
-			return
-		
+	@cost_check(extra=True)
+	async def blackjack(self, ctx: ApplicationContext, bet: int) -> None:
+		player: Player = ctx.player  # type: ignore
 		streak = await BlackJack().play(bet, ctx)
-		player.balance += streak
+		player.balance += streak - bet
 		
 		await ctx.respond(
 			embed=embeds.simple_embed(
