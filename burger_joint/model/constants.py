ce2dca32d99ad3f1308e8948cc105ac1a6c0ea6f
petepoop <70098997+petepoop@users.noreply.github.com>
2025-10-30 22:53:16 -0400

    Added more food items to the menu

--- burger_joint/model/constants.py
+++ burger_joint/model/constants.py
@@ -45,20 +45,67 @@ ALL_UPGRADES: dict[UpgradeID, Upgrade] = {
 }
 
 ALL_FOOD_ITEMS: dict[FoodItemID, FoodItem] = {
+	FoodItemID.CHEESE_BURGER: FoodItem(
+		FoodItemID.CHEESE_BURGER.value, 10, 1, 18, FoodCategoryID.BURGERS
+	),
+	FoodItemID.DOUBLE_CHEESE_BURGER: FoodItem(
+		FoodItemID.DOUBLE_CHEESE_BURGER.value, 14, 2, 25, FoodCategoryID.BURGERS
+	),
 	FoodItemID.CLASSIC_BURGER: FoodItem(
 		FoodItemID.CLASSIC_BURGER.value, 12, 1, 20, FoodCategoryID.BURGERS
 	),
+	FoodItemID.VEGGIE_BURGER: FoodItem(
+		FoodItemID.VEGGIE_BURGER.value, 13, 2, 15, FoodCategoryID.BURGERS
+	),
+	FoodItemID.CHICKEN_SANDWICH: FoodItem(
+		FoodItemID.CHICKEN_SANDWICH.value, 11, 2, 17, FoodCategoryID.BURGERS
+	),
+	FoodItemID.SPICY_CHICKEN_SANDWICH: FoodItem(
+		FoodItemID.SPICY_CHICKEN_SANDWICH.value, 12, 3, 18, FoodCategoryID.BURGERS
+	),
+	FoodItemID.BACON_BURGER: FoodItem(
+		FoodItemID.BACON_BURGER.value, 13, 3, 23, FoodCategoryID.BURGERS
+	),
+	
 	FoodItemID.FRIES: FoodItem(
 		FoodItemID.FRIES.value, 4, 1, 10, FoodCategoryID.SNACKS
 	),
+	FoodItemID.CURLY_FRIES: FoodItem(
+		FoodItemID.CURLY_FRIES.value, 5, 2, 12, FoodCategoryID.SNACKS
+	),
+	FoodItemID.ONION_RINGS: FoodItem(
+		FoodItemID.ONION_RINGS.value, 6, 2, 11, FoodCategoryID.SNACKS
+	),
+	FoodItemID.MOZZARELLA_STICKS: FoodItem(
+		FoodItemID.MOZZARELLA_STICKS.value, 7, 3, 13, FoodCategoryID.SNACKS
+	),
+	FoodItemID.CHICKEN_NUGGETS: FoodItem(
+		FoodItemID.CHICKEN_NUGGETS.value, 6, 1, 12, FoodCategoryID.SNACKS
+	),
+	FoodItemID.SIDE_SALAD: FoodItem(
+		FoodItemID.SIDE_SALAD.value, 5, 1, 8, FoodCategoryID.SNACKS
+	),
+	FoodItemID.APPLE_PIE: FoodItem(
+		FoodItemID.APPLE_PIE.value, 4, 2, 9, FoodCategoryID.SNACKS
+	),
+
 	FoodItemID.SODA: FoodItem(
 		FoodItemID.SODA.value, 2, 1, 6, FoodCategoryID.DRINKS
 	),
-	FoodItemID.VEGGIE_BURGER: FoodItem(
-		FoodItemID.VEGGIE_BURGER.value, 14, 2, 15, FoodCategoryID.BURGERS
+	FoodItemID.ICED_TEA: FoodItem(
+		FoodItemID.ICED_TEA.value, 3, 1, 5, FoodCategoryID.DRINKS
 	),
-	FoodItemID.CHICKEN_SANDWICH: FoodItem(
-		FoodItemID.CHICKEN_SANDWICH.value, 11, 2, 15, FoodCategoryID.BURGERS
+	FoodItemID.LEMONADE: FoodItem(
+		FoodItemID.LEMONADE.value, 3, 2, 7, FoodCategoryID.DRINKS
+	),
+	FoodItemID.MILKSHAKE: FoodItem(
+		FoodItemID.MILKSHAKE.value, 5, 2, 12, FoodCategoryID.DRINKS
+	),
+	FoodItemID.COFFEE: FoodItem(
+		FoodItemID.COFFEE.value, 3, 1, 4, FoodCategoryID.DRINKS
+	),
+	FoodItemID.WATER: FoodItem(
+		FoodItemID.WATER.value, 1, 1, 0, FoodCategoryID.DRINKS
 	)
 }
 
